044bde1d3cb3eb5aee0681d49905fb234ef54731
Shree Murugesan <smurugesan@brontecollege.ca>
2020-03-12 16:05:57 -0400

    Edit Course

--- railsSIS/app/controllers/courses_controller.rb
+++ railsSIS/app/controllers/courses_controller.rb
@@ -6,6 +6,10 @@ class CoursesController < ApplicationController
     def new
     end
 
+    def edit
+        @course = Course.find(params[:id])
+    end
+
     def show
         @course = Course.find(params[:id])
     end
@@ -16,8 +20,17 @@ class CoursesController < ApplicationController
         redirect_to @course
     end
 
+    def update
+        @course = Course.find(params[:id])
+        if @course.update(course_params)
+            redirect_to @course
+          else
+            render 'edit'
+          end
+    end
+
     private
         def course_params
-            params.require(:courses).permit(:title, :code, :teacher, :credits, :description)
+            params.require(:course).permit(:title, :code, :teacher, :credits, :description)
         end
 end
