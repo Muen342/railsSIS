f5f2ec62a0be5e04d3fb9ababa48a087b476bba1
mzhang <mzhang@brontecollege.ca>
2020-03-13 12:20:31 -0400

    added features for students

--- railsSIS/app/controllers/sstudents_controller.rb
+++ railsSIS/app/controllers/sstudents_controller.rb
@@ -1,5 +1,13 @@
 class SstudentsController < ApplicationController
+    def index
+        @sstudents = Sstudent.all
+    end
     def new
+        @sstudent = Sstudent.new
+    end
+    
+    def edit
+        @sstudent = Sstudent.find(params[:id])
     end
     def show
         @sstudent = Sstudent.find(params[:id])
@@ -7,13 +15,30 @@ class SstudentsController < ApplicationController
     def create
         @sstudent = Sstudent.new(sstudent_params)
    
-        @sstudent.save
-        redirect_to @sstudent
+        if @sstudent.save
+            redirect_to @sstudent
+        else
+            render 'new'
+        end
+    end
+    def update
+        @sstudent = Sstudent.find(params[:id])
+        if @sstudent.update(sstudent_params)
+            redirect_to @sstudent
+          else
+            render 'edit'
+          end
+    end
+    def destroy
+        @sstudent = Sstudent.find(params[:id])
+        @sstudent.destroy
+    
+        redirect_to sstudents_path
     end
    
     private
         def sstudent_params
             params.require(:sstudent).permit(:id, :name, :surname, :grade)
         end
- end
+end
  
